5cd987dbad76f8676d2705a1f1fd7a04a67ffa77
MargauxThirion <thirion.margaux@gmail.com>
2024-12-12 14:14:37 +0100

    mongoDB URI en clair

--- src/main/java/com/back_cats/repositories/UserRepository.java
+++ src/main/java/com/back_cats/repositories/UserRepository.java
@@ -4,7 +4,9 @@ package com.back_cats.repositories;
 import com.back_cats.models.User;
 import org.bson.types.ObjectId;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository extends MongoRepository<User, ObjectId> {
     User findByMail(String mail);
 }
--- src/main/java/com/back_cats/repositories/VoitureRepository.java
+++ src/main/java/com/back_cats/repositories/VoitureRepository.java
@@ -3,7 +3,9 @@ package com.back_cats.repositories;
 import com.back_cats.models.Voiture;
 import org.bson.types.ObjectId;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface VoitureRepository extends MongoRepository<Voiture, ObjectId> {
 
 }
